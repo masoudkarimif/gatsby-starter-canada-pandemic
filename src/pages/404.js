b8879e074cf986b8c354dc350b670515cfb38ae5
masoudkarimif <karimif.masoud@gmail.com>
2020-03-30 07:31:46 -0600

    Added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,20 @@
 import React from "react"
+import Layout from "../components/layout"
+import {useStaticQuery, graphql} from 'gatsby'
 
 export default (props)=> {
+const data = useStaticQuery(graphql`
+          query  {
+            site {
+              siteMetadata {
+                  name
+              }
+            }
+          }
+        `)
   return (
-    <h1>Not Found</h1>
+      <Layout pageTitle={data.site.siteMetadata.name} pageDescription="Page Not Found">
+      </Layout>
+
   )
 }
