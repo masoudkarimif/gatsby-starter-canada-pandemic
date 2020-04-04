5a3fb1902010dc4f47f13e9556e406ea786e7e9d
masoudkarimif <karimif.masoud@gmail.com>
2020-04-04 12:36:47 -0600

    Deleted comments

--- src/components/map.js
+++ src/components/map.js
@@ -27,59 +27,58 @@ const Popup = (props) => {
 
 const CanadaMap = ({mapData, popupData, width, height, scale, mouseLeaveHandler, mouseMoveHandler, mouseEnterHandler}) => {
 
-    const svgRef = useRef(null)
-    let ratio = 0
-    const totalCases = popupData.reduce((total, item)=> total+parseInt(item.cases), 0);
+      const svgRef = useRef(null)
+      let ratio = 0
+      const totalCases = popupData.reduce((total, item)=> total+parseInt(item.cases), 0);
 
-    const fillColor = number => {
-      if (number < 50) {
-        return "#ddd";
+      const fillColor = number => {
+        if (number < 50) {
+          return "#ddd";
+        }
+        const h = 0;
+        let s = 100;
+        let l = 95 - (number / totalCases) * 100
+        s /= 100;
+        l /= 100;
+
+        let c = (1 - Math.abs(2 * l - 1)) * s,
+            x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+            m = l - c/2,
+            r = 0,
+            g = 0,
+            b = 0;
+
+        if (0 <= h && h < 60) {
+          r = c; g = x; b = 0;
+        } else if (60 <= h && h < 120) {
+          r = x; g = c; b = 0;
+        } else if (120 <= h && h < 180) {
+          r = 0; g = c; b = x;
+        } else if (180 <= h && h < 240) {
+          r = 0; g = x; b = c;
+        } else if (240 <= h && h < 300) {
+          r = x; g = 0; b = c;
+        } else if (300 <= h && h < 360) {
+          r = c; g = 0; b = x;
+        }
+        r = Math.round((r + m) * 255).toString(16);
+        g = Math.round((g + m) * 255).toString(16);
+        b = Math.round((b + m) * 255).toString(16);
+
+        if (r.length === 1)
+          r = "0" + r;
+        if (g.length === 1)
+          g = "0" + g;
+        if (b.length === 1)
+          b = "0" + b;
+
+        return "#" + r + g + b;
       }
-      const h = 0;
-      let s = 100;
-      let l = 95 - (number / totalCases) * 100
-      s /= 100;
-      l /= 100;
 
-      let c = (1 - Math.abs(2 * l - 1)) * s,
-          x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-          m = l - c/2,
-          r = 0,
-          g = 0,
-          b = 0;
-
-      if (0 <= h && h < 60) {
-        r = c; g = x; b = 0;
-      } else if (60 <= h && h < 120) {
-        r = x; g = c; b = 0;
-      } else if (120 <= h && h < 180) {
-        r = 0; g = c; b = x;
-      } else if (180 <= h && h < 240) {
-        r = 0; g = x; b = c;
-      } else if (240 <= h && h < 300) {
-        r = x; g = 0; b = c;
-      } else if (300 <= h && h < 360) {
-        r = c; g = 0; b = x;
-      }
-      r = Math.round((r + m) * 255).toString(16);
-      g = Math.round((g + m) * 255).toString(16);
-      b = Math.round((b + m) * 255).toString(16);
-
-      if (r.length === 1)
-        r = "0" + r;
-      if (g.length === 1)
-        g = "0" + g;
-      if (b.length === 1)
-        b = "0" + b;
-
-      return "#" + r + g + b;
-    }
-
-    const createThePlot = ()=> {
+      const createThePlot = ()=> {
 
       const w = width
       const h = height
-      //const self = this
 
       const svg = d3.select(svgRef.current).append("svg")
         .attr("width", "100%")
@@ -145,21 +144,11 @@ const CanadaMap = ({mapData, popupData, width, height, scale, mouseLeaveHandler,
           .on("mousemove", function(e){
                 mouseMoveHandler(d3.mouse(this)[0] * ratio, d3.mouse(this)[1] * ratio)
             })
-
-
     }
 
     useEffect(()=>{
         createThePlot()
     }, [])
-
-    //componentDidMount() {
-    //  this.createThePlot()
-    //}
-//
-    //componentDidUpdate() {
-    //  ratio = (svgRef.current.getBoundingClientRect().height / height)
-    //}
 
       return (
         <div ref={svgRef} id="svg-holder" className="column"></div>
